a1492d14a63454ed7848dfa8b7b11c6b9e355e4a
JayBassCodes <jacob.m.bassett@gmail.com>
2023-07-10 17:39:09 -0400

    added script to generate time blocks for 9-5, added event listener for saving info to local storage added dayjs to keep current time displayed as well as past/present/future color coding for day planner

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
 
+$(function () {
+  function createTimeBlocks() {
+    var now = dayjs();
+    var currentHour = now.hour();
+
+    var containerEl = $(".container-lg");
+    containerEl.empty();
+
+    // for loop to 9-5 based on 24hr time
   for (let hour = 9; hour <= 17; hour++) {
     let timeBlockStatus = "";
     // time block status to be determined
@@ -13,29 +18,68 @@ $(function () {
     } else {
         timeBlockStatus = "future";
     }
+
     // AM/PM code
-    const hourLabel = hour > 12 ? `${hour - 12}PM` : hour === 12 ? `${hour}PM` : `${hour}AM`;
+    var hourLabel = hour > 12 ? `${hour - 12}PM` : hour === 12 ? `${hour}PM` : `${hour}AM`;
      // hour id
-     const id = `hour-${hour}`;
-     // Checks local storage for saved data
+     var id = `hour-${hour}`;
+     // checks local storage for saved data
      let description = getSavedTimeBlock(id);
-     
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+     var timeBlock = `
+          <div id="${id}" class="row time-block ${timeBlockStatus}">
+          <div class="col-2 col-md-1 hour text-center py-3">${hourLabel}</div>
+          <textarea class="col-8 col-md-10 description" rows="3">${description}</textarea>
+          <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+              <i class="fas fa-save" aria-hidden="true"></i>
+          </button>
+          </div>
+          `;
+          // adds time blockl to the container
+          containerEl.append(timeBlock);
+        }
+        const timeRemaining = 60 - Number(now.minute());
+        setTimeout(createTimeBlocks, timeRemaining * 1000 * 60);
+      }
+    
+  
+        function onSaveClick(event) {
+          var saveButton = $(this);
+          // id of the closest time-block
+          var timeBlockId = saveButton.closest(".time-block").attr("id");
+          // description value
+          var description = $.trim(saveButton.siblings("textarea").val());
+    
+          // if the description is empty then remove it from local storage
+          if (description === "") {
+              localStorage.removeItem(timeBlockId);
+              return;
+          }
+          // determines what to save
+          var itemToSave = {
+            description: description,
+        };
+        // saves to local storage
+        localStorage.setItem(timeBlockId, JSON.stringify(itemToSave));
+  };
+
+  function getSavedTimeBlock(timeBlockId) {
+    // pull from local storage
+    var savedItem = localStorage.getItem(timeBlockId);
+    // if empty return empty string otherwise return the description
+    return savedItem ? JSON.parse(savedItem).description : "";
+}
+
+function setCurrentDayHeader() {
+  // get the current date using dayjs
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+
+  $("#currentDay").text(currentDate);
+}
+
+setCurrentDayHeader();
+  createTimeBlocks();
+
+  $(".saveBtn").on("click", onSaveClick);
+
 });
